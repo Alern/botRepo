923a6087109e0a5dfaeecffb193215ff8aecb9a7
Alern <alerncheruiyot@gmail.com>
2017-03-24 07:09:07 +0300

    editing code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 app.post("/webhook", function (req, res) {
   // Make sure this is a page subscription
   if (req.body.object == "page") {
@@ -63,27 +64,5 @@ function sendMessage(recipientId, message) {
 }
 const http = require('http');
 const Bot = require('messenger-bot');
-
-let bot = new Bot({
-    token: process.env.PAGE_TOKEN || '',
-    verify: process.env.VERIFY_TOKEN || '',
-});
-
-bot.on('error', (err) => {
-    console.log(err.message)
-});
-
-bot.on('message', (payload, reply) => {
-    let text = payload.message.text;
-
-    reply({ text: 'Hello, I am an interactive bot :)' }, function(err) {
-        if (err) console.log(err);
-    });
- reply({ text: 'i talk less though ;-)' }, function(err) {
-        if (err) console.log(err);
-    });
-
-});
-
 let port = process.env.PORT || 3000;
 http.createServer(bot.middleware()).listen(port);
